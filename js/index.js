e7e4779ce15b6a9a2dad8fc015a8309eaa29c43a
Jhan Frank <JhnFrankz@gmail.com>
2022-04-23 16:28:23 -0500

    Inicio de validacion de formulario sobre nosotros

---
+++ js/index.js
@@ -0,0 +1,26 @@
+/*
+const name = document.querySelector("[data-name]");
+const message = document.querySelector("[data-message]");
+
+const btnAboutUs = document.querySelector("")
+*/
+
+/*
+    El formulario de contacto debe contener algunas validaciones como:
+
+    Campo de nombre: máximo 40 caracteres.
+    Campo de mensaje: máximo 120 caracteres.
+    Validación para que los campos no queden vacíos o en blanco.
+    Debe mostrar al usuario los mensajes de error en caso de que no se cumplan las validaciones.
+    Botón Enviar mensaje: envía el mensaje si el formulario está correctamente cumplimentado.
+*/
+
+const inputs = document.querySelectorAll(".form__about-us input");
+
+inputs.forEach(input => {
+    input.addEventListener("blur", (input) => {
+        console.log(input.target)
+        //validate(input);
+    });
+});
+
