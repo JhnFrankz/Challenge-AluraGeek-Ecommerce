942383956246f5f167f2737259ab1986898651b5
Jhan Frank <JhnFrankz@gmail.com>
2022-05-03 10:58:57 -0500

    Conexion con json y avance del metodo get

---
+++ controllers/product-controller.js
@@ -0,0 +1,25 @@
+import { productServices } from "../service/product-service.js";
+
+const crearNuevoProducto = (nombre, precio, id) => {
+    const contenido = `
+        <img src="img/product${id}.png" alt="Imagen producto ${nombre}">
+        <p>${nombre}</p>
+        <p class="product__precio">$${precio.toFixed(2)}</p>
+        <p>#${id}</p>`;
+
+    const producto = document.createElement("div");
+    producto.classList.add("product__item");
+    producto.innerHTML = contenido;
+
+    return producto;
+};
+
+
+const listaProductos = document.querySelector("[data-products-list]");
+
+productServices.listaProductos().then((data) => {
+    data.forEach(({nombre, precio, id}) => {
+        const nuevoProducto = crearNuevoProducto(nombre, precio, id);
+        listaProductos.appendChild(nuevoProducto);
+    });
+}).catch(error => console.log("Hubo un error al crear un nuevo Producto: \n", error));
