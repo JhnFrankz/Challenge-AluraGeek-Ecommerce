51179b9c131c516c2adfcb6ac5c29129976a639d
Jhan Frank <JhnFrankz@gmail.com>
2022-06-02 13:04:44 -0500

    Terminada funcion para seleccionar y previsualizar imagenes seleccionadas

--- controllers/index.js
+++ controllers/index.js
@@ -18,7 +18,30 @@ dragZone.addEventListener("dragleave", (e) => {
     dragZone.classList.remove("form__file__result--active");
 });
 
+const uploadImage = (file) => {
+    //Leemos los archivos que nos llegan (file)
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    
+    fileReader.addEventListener("load", (e) => {
+        img.setAttribute("src", e.target.result);
+    });
+};
+
 //Al sortar una imagen en el campo no abrir en una pestaña nueva
 dragZone.addEventListener("drop", (e) => {
     e.preventDefault();
+    //asignamos al fileInput la imagen que arrastramos en el campo
+    fileInput.files = e.dataTransfer.files;
+    const file = fileInput.files[0];
+    
+    uploadImage(file);
+});
+
+//cada vez que cambie el archivo que se le pase hace..
+fileInput.addEventListener("change", (e) => {
+    //capturamos el archivo que se le pase
+    const file = e.target.files[0];
+
+    uploadImage(file);
 });
