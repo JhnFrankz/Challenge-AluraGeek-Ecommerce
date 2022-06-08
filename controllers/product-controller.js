c51d0380e36c778ec35e036ea732a2b9e260a22a
Jhan Frank <JhnFrankz@gmail.com>
2022-06-08 16:28:25 -0500

    Buttons for edit and delete in all-products added

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,6 +1,6 @@
 import { productServices } from "../service/product-service.js";
 
-const crearNuevoProducto = (nombre, precio, id) => {
+/*const crearNuevoProducto = (nombre, precio, id) => {
     const contenido = `
         <a href="product.html?id=${id}">
             <img src="img/product${id}.png" alt="Imagen producto ${id}">
@@ -14,8 +14,40 @@ const crearNuevoProducto = (nombre, precio, id) => {
     producto.innerHTML = contenido;
 
     return producto;
-};
+};*/
 
+const crearNuevoProducto = (nombre, precio, id) => {
+    const contenido = `
+    <div class="product__box">
+        <div class="product__box__properties">
+            <div class="product__property">
+                <a href="edit-product.html?id=${id}"><i class="fa-solid fa-pen"></i></a>
+            </div>
+            <div class="product__property">
+                <a class="btn-delete" href="all-products.html" id="${id}"><i class="fa-solid fa-trash"></i></a>
+            </div>
+        </div>
+        <img src="img/product${id}.png" alt="Imagen producto ${id}">
+    </div>
+    <p>${nombre}</p>
+    <p class="product__precio">$${parseFloat(precio).toFixed(2)}</p>
+    <p>#${id}</p>`;
+
+    const producto = document.createElement("div");
+    producto.classList.add("product__item");
+    producto.innerHTML = contenido;
+    
+    const btnEliminar = producto.querySelector(".btn-delete");
+    btnEliminar.addEventListener("click", () => {
+        const id = btnEliminar.id;
+        productServices.eliminarProducto(id).then(respuesta => {
+            console.log(respuesta);
+        }).catch(error => console.log("Ocurrió un error"));
+
+    });
+
+    return producto;
+};
 
 const listaProductos = document.querySelector("[data-products-list]");
 
