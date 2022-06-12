70fb617ac7d34ff664367e9987a3d3aba26b7425
Jhan Frank <JhnFrankz@gmail.com>
2022-06-11 22:49:32 -0500

    Avance para mostrar productos similares al ver detalles de un producto

--- controllers/product-information-controller.js
+++ controllers/product-information-controller.js
@@ -19,6 +19,20 @@ const crearSeccionProducto = (nombre, precio, descripcion, id) => {
     return producto;
 };
 
+const crearProductoSimilar = (imagen, nombre, precio, id) => {
+    const contenido = `
+    <img src="${imagen}" alt="Imagen producto ${id}">
+    <p>${nombre}</p>
+    <p class="product__precio">$${parseFloat(precio).toFixed(2)}</p>
+    <a href="product.html?id=${id}">Ver producto</a>`;
+    
+    const producto = document.createElement("div");
+    producto.classList.add("product__item");
+    producto.innerHTML = contenido;
+    
+    return producto;
+};
+
 
 const obtenerInformacion = () => {
     const url = new URL(window.location);
@@ -29,6 +43,25 @@ const obtenerInformacion = () => {
         const infoProducto = crearSeccionProducto(nombre, precio, descripcion, id);
         producto.appendChild(infoProducto);
     });
+
+    const productosSimilares = document.querySelector("[data-similar-products]");
+
+    productServices.listaProductos().then((data) => {
+        let count = 0;
+
+        data.forEach(({imagen, nombre, precio, id}) => {
+            if (count >= 6) {
+                return;
+            }
+
+            if (id != idUrl) {
+                const nuevoProducto = crearProductoSimilar(imagen, nombre, precio, id);
+                productosSimilares.appendChild(nuevoProducto);
+                count++;
+            }
+        });
+    }).catch(error => console.log("Hubo un error al crear un nuevo Producto: \n", error));
+    
 }
 
 obtenerInformacion();
